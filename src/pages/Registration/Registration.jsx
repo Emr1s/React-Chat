4abb0dddd8d09ca642364108724e1eb332370e7d
Nazarii Harmaliuk <nazargamaluyk@gmail.com>
2023-08-03 09:09:02 +0300

    added loader state for implemntation async code

--- src/pages/Registration/Registration.jsx
+++ src/pages/Registration/Registration.jsx
@@ -6,10 +6,12 @@ import { auth, storage, db } from '../../firebase'
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
+import { ClipLoader } from "react-spinners";
 
 
 const Register = () => {
   const [error, setError] = useState(false)
+  const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
 
   const halndleSubmit = async (e) => {
@@ -21,23 +23,24 @@ const Register = () => {
     const confirmPassword = e.target[4].value
     const avatar = e.target[5].files[0]
 
+    setLoading(true)
     try {
       const res = await createUserWithEmailAndPassword(auth, email, password)
 
       const storageRef = ref(storage, name);
-      
+
       const uploadTask = uploadBytesResumable(storageRef, avatar);
-      
-      uploadTask.on('state_changed', 
+
+      uploadTask.on('state_changed',
         (snapshot) => {
-        }, 
+        },
         (error) => {
-         setError(true)
-        }, 
+          setError(true)
+        },
         () => {
-          getDownloadURL(uploadTask.snapshot.ref).then( async (downloadURL) => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
             console.log('File available at', downloadURL);
-           await updateProfile(res.user, {
+            await updateProfile(res.user, {
               displayName: name,
               photoURL: downloadURL
             });
@@ -49,38 +52,43 @@ const Register = () => {
             });
             await setDoc(doc(db, "userChats", res.user.uid), {});
             navigate('/');
+            setLoading(false)
           });
         }
       );
     } catch (error) {
       setError(true)
-
+      setLoading(false)
     }
 
   }
   return (
     <div className='registration'>
-      <form className="registration-form" onSubmit={halndleSubmit}>
-        <h1>Registration</h1>
-        <input type="text" placeholder="Name" />
-        <input type="text" placeholder="Second Name" />
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <input type="password" placeholder="Confirm Password" />
-        <div className="photo">
-          <span>Add your photo</span>
-          <input type="file" placeholder="Confirm Password" />
-        </div>
-        <div className="loginback">
-          <Link to={'/login'}>
-            <p>Don't have an account?</p>
-          </Link>
-        </div>
-        <div className="btn">
-          <button>Sign up</button>
-          {error && <span>Something went wrong</span>}
-        </div>
-      </form>
+      {loading ? <ClipLoader />
+        :
+
+        <form className="registration-form" onSubmit={halndleSubmit}>
+          <h1>Registration</h1>
+          <input type="text" placeholder="Name" />
+          <input type="text" placeholder="Second Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Confirm Password" />
+          <div className="photo">
+            <span>Add your photo</span>
+            <input type="file" placeholder="Confirm Password" />
+          </div>
+          <div className="loginback">
+            <Link to={'/login'}>
+              <p>Already have an account?</p>
+            </Link>
+          </div>
+          <div className="btn">
+            <button>Sign up</button>
+            {error && <span>Something went wrong</span>}
+          </div>
+        </form>
+      }
     </div>
   )
 }

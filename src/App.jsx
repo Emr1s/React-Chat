a346533bf764301646b1d46bb3659696d0fc39b2
Nazarii Harmaliuk <nazargamaluyk@gmail.com>
2023-08-03 09:04:09 +0300

    added auth validation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import ChatPage from './pages/ChatPage/ChatPage'
 import Login from './pages/Login/Login'
 import Registration from './pages/Registration/Registration'
+import { useContext } from 'react'
+import { AuthContext } from './context/context'
 function App() {
 
+  const { user } = useContext(AuthContext)
+  const ProtectedAuth = ({ children }) => {
+    if(!user){
+      return <Navigate to={'/login'} />
+    }
+    return children;
+  }
   return (
     <>
       <BrowserRouter>
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/registration" element={<Registration />} />
-          <Route path="/" element={<ChatPage />} />
+          <Route path="/" element={
+            <ProtectedAuth>
+              <ChatPage />
+            </ProtectedAuth>} />
           <Route path='/*' element={<Login />} />
         </Routes>
       </BrowserRouter>

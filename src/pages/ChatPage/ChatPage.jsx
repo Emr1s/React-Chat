0564e57694d1270deff8e091a68288ae1d151b16
Nazarii Harmaliuk <nazargamaluyk@gmail.com>
2023-08-03 19:49:37 +0300

    component destructuring

--- src/pages/ChatPage/ChatPage.jsx
+++ src/pages/ChatPage/ChatPage.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import './ChatPage.scss'
-import ChatMessage from '../../components/Chat/ChatMessage'
-import { BsCameraVideo } from 'react-icons/bs'
-import { FaUserPlus } from 'react-icons/fa'
-import { BsThreeDots } from 'react-icons/bs'
-import { CiImageOn } from 'react-icons/ci'
-import { IoIosAttach } from 'react-icons/io'
-import Message from '../../components/Message/Message'
+import ChatMessage from '../../components/ChatMessage/ChatMessage'
+
 
 import Nav from '../../components/Nav/Nav'
+import ChatWindow from '../../components/ChatWindow/ChatWindow'
 const ChatPage = () => {
   return (
     <div className='chatpage'>
@@ -17,38 +13,10 @@ const ChatPage = () => {
           <Nav />
           <hr />
           <div className="chat">
-            <div className="message">
-              <ChatMessage />
-              <ChatMessage />
-              <ChatMessage />
-            </div>
+            <ChatMessage />
           </div>
         </div>
-        <div className="right">
-          <div className="navbar">
-            <span style={{ fontSize: '18px' }}>Oleg</span>
-            <div className="icons">
-              <BsCameraVideo style={{ cursor: 'pointer' }} />
-              <FaUserPlus style={{ cursor: 'pointer' }} />
-              <BsThreeDots style={{ cursor: 'pointer' }} />
-            </div>
-          </div>
-          <div className="messages">
-            <Message />
-            <Message />
-          </div>
-          <div className="inputMessage">
-            <div className="input">
-              <input type="text" placeholder='Type a message...' />
-            </div>
-            <div className="chatIcons">
-              <input type="file" style={{ display: 'none' }} id='file' />
-              <IoIosAttach />
-              <CiImageOn />
-              <button>Send</button>
-            </div>
-          </div>
-        </div>
+       <ChatWindow />
       </div>
     </div>
   )
